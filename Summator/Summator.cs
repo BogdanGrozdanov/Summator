092207e16e97955f3f0c026dd8d6bb7a7cc6e05e
BogdanGrozdanov <64480248+BogdanGrozdanov@users.noreply.github.com>
2022-05-14 23:37:50 +0300

    Add Summator folder

---
+++ Summator/Summator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Summator
+    {
+        static void Main(string[] args)
+        {
+            var arr = new long[] { 1,1};
+            Console.WriteLine(Sum(arr));
+        }
+        public static long Sum(long[] arr)
+        {
+            long sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+            return sum;
+        }
+
+        public static double Average(double[] arr)
+        {
+            double sum = 0;
+            for (int i = 1; i < arr
+              .Length; i++)
+                sum += arr[i];
+            return sum / arr.Length;
+        }
+
+    }
+}
